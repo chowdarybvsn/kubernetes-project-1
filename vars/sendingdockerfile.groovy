7bc35c1f2bcf130ce2c9722450de4026ecf75145
chowdarybvsn@gmail.com <chowdarybvsn@gmail.com>
2023-07-01 14:22:17 +0530

    only copying dockerfile

--- vars/sendingdockerfile.groovy
+++ vars/sendingdockerfile.groovy
@@ -1,4 +1,4 @@
 def call(){
 sh 'ssh -o StrictHostKeyChecking=no ansadmin@10.0.1.45'
-sh 'scp /var/lib/jenkins/workspace/kubernetes/* ansadmin@10.0.1.45:/home/ansadmin'
+sh 'scp /var/lib/jenkins/workspace/kubernetes/Docker* ansadmin@10.0.1.45:/home/ansadmin'
 }
